0cf3894a9919e02126c7d1cf6ef2ceecfdc672c5
Tyreese Green <146450901+Tyreese2070@users.noreply.github.com>
2024-11-26 23:23:42 +0000

    added login page

--- movie-recommendation-app/app/__init__.py
+++ movie-recommendation-app/app/__init__.py
@@ -1,4 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-from app import views
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+login_manager.login_view = 'login'
+login_manager.login_message = 'Please log in to access this page.'
+login_manager.login_message_category = 'info'
+
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+from app import views, models
+from app.models import User
