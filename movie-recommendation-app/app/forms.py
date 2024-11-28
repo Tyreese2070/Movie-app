86e8c7a24bd15e168b519904df80ecd53d3d4f75
Tyreese Green <146450901+Tyreese2070@users.noreply.github.com>
2024-11-28 00:37:47 +0000

    updated movie details page

--- movie-recommendation-app/app/forms.py
+++ movie-recommendation-app/app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, IntegerField, TextAreaField
+from wtforms.validators import DataRequired, NumberRange
 
 class loginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -11,3 +11,11 @@ class signupForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign up')
+    
+class likeForm(FlaskForm):
+    submit = SubmitField('Like')
+
+class reviewForm(FlaskForm):
+    rating = IntegerField('Rating', validators=[DataRequired(), NumberRange(min=1, max=10)])
+    review_text = TextAreaField('Review')
+    submit = SubmitField('Submit Review')
--- movie-recommendation-app/app/views.py
+++ movie-recommendation-app/app/views.py
@@ -1,11 +1,11 @@
 from flask import render_template, redirect, url_for, flash, request, jsonify
 from flask_login import login_user, logout_user, login_required, current_user
-from app.models import User
-from app.forms import loginForm
-from app.forms import signupForm
+from app.models import User, Movie, Review, Like
+from app.forms import loginForm, signupForm, likeForm, reviewForm
 import requests
 from app import app, db
 from flask_bcrypt import Bcrypt
+from datetime import datetime
 
 bcrypt = Bcrypt(app)
 
@@ -106,18 +106,53 @@ URL = 'https://api.themoviedb.org/3'
 def homepage():
     """
     Displays movies for the user to see.
-
-    Returns:
-        - homepage template
     """
-
     url = f'{URL}/movie/popular?api_key={KEY}'
     response = requests.get(url)
     data = response.json()
 
     movies = data.get('results', [])
+    for movie_data in movies:
+        existing_movie = Movie.query.filter_by(id=movie_data['id']).first()
+        if not existing_movie:
+            release_date = datetime.strptime(movie_data['release_date'], "%Y-%m-%d").date()
+
+            new_movie = Movie(
+                id=movie_data['id'],
+                title=movie_data['title'],
+                genre=', '.join(str(genre_id) for genre_id in movie_data.get('genre_ids', [])),
+                release_date=release_date
+            )
+            db.session.add(new_movie)
+    db.session.commit()
 
     return render_template('homepage.html', movies=movies)
+
+@app.route('/like_movie/<int:movie_id>', methods=['POST'])
+@login_required
+def like_movie(movie_id):
+    """
+    Handles liking a movie for the user.
+
+    Parameters:
+        - movie_id: ID of the movie to be liked.
+    
+    Returns:
+        - Redirect to the homepage with updated like status.
+    """
+    movie = Movie.query.get_or_404(movie_id)
+    
+    existing_like = Like.query.filter_by(user_id=current_user.id, movie_id=movie.id).first()
+    if not existing_like:
+        like = Like(user_id=current_user.id, movie_id=movie.id)
+        db.session.add(like)
+        db.session.commit()
+        flash(f'You liked {movie.title}!', 'success')
+    else:
+        flash(f'You already liked {movie.title}.', 'info')
+    
+    return redirect(url_for('homepage'))
+
 
 @app.route('/likes')
 @login_required
@@ -147,7 +182,41 @@ def dashboard():
 def settings():
     return render_template('settings.html')
 
-@app.route('/movie')
+@app.route('/movie/<int:movie_id>')
 @login_required
-def movie():
-    return render_template('movie.html')
+def movie(movie_id):
+    """
+    Displays the movie details and handles liking and reviewing.
+    """
+    # Query the movie from the database using the movie_id
+    movie = Movie.query.get_or_404(movie_id)
+
+    # Forms for liking and reviewing
+    like_form = likeForm()
+    review_form = reviewForm()
+
+    # Handle liking the movie
+    if like_form.validate_on_submit() and like_form.submit.data:
+        flash(f'You liked {movie.title}!', 'success')
+        # Handle liking functionality (e.g., save in DB, optional feature)
+        return redirect(url_for('movie', movie_id=movie.id))
+
+    # Handle submitting a review
+    if review_form.validate_on_submit() and review_form.submit.data:
+        new_review = Review(
+            user_id=current_user.id,
+            movie_id=movie.id,
+            rating=review_form.rating.data,
+            review_text=review_form.review_text.data
+        )
+        db.session.add(new_review)
+        db.session.commit()
+        flash('Your review has been submitted!', 'success')
+        return redirect(url_for('movie', movie_id=movie.id))
+
+    return render_template(
+        'movie.html',
+        movie=movie,
+        like_form=like_form,
+        review_form=review_form
+    )
