cc232706adcea59902d8a3d12b36d555793f1530
Tyreese Green <146450901+Tyreese2070@users.noreply.github.com>
2024-12-03 17:04:03 +0000

    cleaned up code, added comments, added search bar functionality

--- movie-recommendation-app/run.py
+++ movie-recommendation-app/run.py
@@ -1,13 +1,4 @@
 from app import app
-from app import db
-
-#if __name__ == '__main__':
-#    app.run(debug=True)
-    
-from socket import gethostname
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    if 'liveconsole' not in gethostname():
-        app.run()
+    app.run(debug=True)
